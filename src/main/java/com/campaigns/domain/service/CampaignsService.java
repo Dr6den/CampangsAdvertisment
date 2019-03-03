7f5b022075af4ce96df802a9407e4261d1bed933
andrew <dr6den@gmail.com>
2019-03-03 22:34:58 +0200

    endpoint rest methods implemented

--- src/main/java/com/campaigns/domain/service/CampaignsService.java
+++ src/main/java/com/campaigns/domain/service/CampaignsService.java
@@ -1,7 +1,10 @@
 package com.campaigns.domain.service;
 
 import com.campaigns.dao.CampaignsDao;
+import com.campaigns.domain.Ad;
 import com.campaigns.domain.Campaign;
+import com.campaigns.domain.Summary;
+import java.util.List;
 import javax.inject.Inject;
 
 /**
@@ -18,5 +21,37 @@ public class CampaignsService {
     
     public Campaign getCampaign(int id) {
         return dao.getCampaignById(id);
+    }
+    
+    public Ad getAd(int id) {
+        return dao.getAdById(id);
+    }
+    
+    public List<Summary> getSummary(int numberOfItems, String filterByName, String filterByStatus, String sortingParameter) {
+        return dao.getSummary(numberOfItems, filterByName, filterByStatus, sortingParameter);
+    }
+    
+    public void deleteCampaign(int id) {
+        dao.deleteCampaign(id);
+    }
+    
+    public void deleteAd(int id) {
+        dao.deleteAd(id);
+    }
+    
+    public int updateCampaign(Campaign cam) {
+        return dao.updateCampaign(cam);
+    }
+    
+    public int updateAd(Ad ad, int campaignId) {
+        return dao.updateAd(ad, campaignId);
+    }
+    
+    public int createCampaign(Campaign cam) {
+        return dao.insertCampaign(cam);
+    }
+    
+    public int createAd(Ad ad, int campaignId) {
+        return dao.insertAd(ad, campaignId);
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,26 +5,8 @@
 <title>Insert title here</title>
 </head>
 <body>
-<div align="center">Get Request Example in WebServices
+<div align="center">Simple web andrew's app
 </div>
-<form action="resource/RegisterProcess" method="get" name="form1" id="form1">
-  <table width="401" border="0" align="center">
-  <tr>
-    <td width="118">&nbsp;</td>
-    <td width="267"><input name="btn_register" type="submit" id="btn_register" value="Register"></td>
-  </tr>
-</table>
-</form>
-<hr>
-<div align="center">Post Request Example in WebServices</div>
-<form action="resource/RegisterProcess" method="post" name="form2" id="form2">
-  <table width="401" border="0" align="center">
-  <tr>
-    <td width="118">&nbsp;</td>
-    <td width="267"><input name="btn_register" type="submit" id="btn_register" value="Register"></td>
-  </tr>
-</table>
-</form>
 <hr>
 
 </body>
